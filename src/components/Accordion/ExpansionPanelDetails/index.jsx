8a4656e370414431965488dcf9e918ce3dee4a22
Matt Tamal <matthew.tamal@gmail.com>
2019-08-02 18:09:35 +0600

    Fixed .idea commit

--- src/components/Accordion/ExpansionPanelDetails/index.jsx
+++ src/components/Accordion/ExpansionPanelDetails/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import classNames from 'classnames';
+import classNames from 'clsx';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import MuiExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
